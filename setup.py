c73a6e7fe63d693710077a484608289c6c3fd703
Aniudupa15 <aniudupa15@gmail.com>
2025-11-25 18:26:13 +0530

    Fix package name for PyPI publish

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="datascience_toolkit",
+    name="datascience_toolkitt",
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
