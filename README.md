070b2500e3ab09e26ce1c66fb46f5c4fb977ce48
Aniudupa15 <aniudupa15@gmail.com>
2025-11-25 18:01:19 +0530

    Prepare package metadata, README, manifest

---
+++ README.md
@@ -0,0 +1,5 @@
+# datascience_toolkit
+
+Usage examples...
+
+![package-screenshot](/mnt/data/12985101-b4ed-41f4-9a7e-acdb56f5e45c.png)
--- datascience_toolkit/__init__.py
+++ datascience_toolkit/__init__.py
@@ -1,5 +1,5 @@
 from .clustering import run_kmeans
 from .apriori_analysis import run_apriori
-from .sentiment_analysis import run_sentiment
+from .sentiment_analysis import run_sentiment_analysis
 from .social_network import run_sna
 from .analysis import run_all
